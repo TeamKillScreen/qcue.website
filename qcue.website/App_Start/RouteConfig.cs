c3921e172e64e20e30199ed8370febb230beada0
Craig Reay <craigcsruk@googlemail.com>
2013-10-26 15:22:42 +0100

    Restructured content and added initial packages such as firebase and angular js

--- qcue.website/App_Start/RouteConfig.cs
+++ qcue.website/App_Start/RouteConfig.cs
@@ -13,11 +13,12 @@ namespace qcue.website
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            // TODO: (CSR) Delete if not needed.
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
