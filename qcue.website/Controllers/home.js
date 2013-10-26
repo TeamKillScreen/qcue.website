a7e99c2e2fe48580b8c180f251a93596f9443f53
Craig Reay <craigcsruk@googlemail.com>
2013-10-26 17:04:19 +0100

    First firebase queues integration

--- qcue.website/Controllers/home.js
+++ qcue.website/Controllers/home.js
@@ -1,2 +1,5 @@
 ﻿function HomeController($scope, angularFire) {
+    var ref = new Firebase("https://qcue-live.firebaseio.com/queues");
+    $scope.queues = [];
+    angularFire(ref, $scope, "queues");
 }
