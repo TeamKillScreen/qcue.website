cd5a71e5cbf2320afeabe712e4e4e3617c27ad90
Craig Reay <craigcsruk@googlemail.com>
2013-10-26 18:37:39 +0100

    Added user details to queue details page.

---
+++ qcue.website/Controllers/user.js
@@ -0,0 +1,7 @@
+﻿function UserController($scope, angularFire) {
+    $scope.init = function (userId) {
+        var ref = new Firebase("https://qcue-live.firebaseio.com/users/" + userId);
+        $scope.user = null;
+        angularFire(ref, $scope, "user");
+    };
+}
