915f477741a6ff3d1ca6cfe7e914baec63699869
Craig Reay <craigcsruk@googlemail.com>
2013-10-26 18:03:16 +0100

    Added queue details page.

--- qcue.website/Scripts/app.js
+++ qcue.website/Scripts/app.js
@@ -4,7 +4,9 @@
             when('/', { templateUrl: 'views/home.html', controller: HomeController }).
             when('/home', { templateUrl: 'views/home.html', controller: HomeController }).
             when('/about', { templateUrl: 'views/about.html', controller: AboutController }).
-            when('/contact', { templateUrl: 'views/contact.html', controller: ContactController });
+            when('/contact', { templateUrl: 'views/contact.html', controller: ContactController }).
+            when('/queue-detail', { templateUrl: 'views/queue-detail.html', controller: QueueDetailController })
+        ;
     });
 
 AppCntl.$inject = ['$scope', '$route']
