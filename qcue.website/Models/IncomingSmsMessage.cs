fcc7875bc19975ac783f3f2afc5f8b0f0fd973e5
Alan Gorton <dev.code1up@googlemail.com>
2013-10-26 15:01:57 +0100

    Added Hello World API controller.

---
+++ qcue.website/Models/IncomingSmsMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace qcue.website.Controllers
+{
+    public class IncomingSmsMessage
+    {
+        [Required]
+        public string Msg_Id { get; set; }
+
+        [Required]
+        public string To { get; set; }
+
+        [Required]
+        public string From { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
