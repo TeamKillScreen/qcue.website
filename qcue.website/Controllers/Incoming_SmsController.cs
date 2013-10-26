3228942bb2ec99df13a228fc9dded17a3f10176b
Alan Gorton <dev.code1up@googlemail.com>
2013-10-26 21:34:30 +0100

    Refactor API code. For the better.

--- qcue.website/Controllers/Incoming_SmsController.cs
+++ qcue.website/Controllers/Incoming_SmsController.cs
@@ -12,6 +12,8 @@ namespace QCue.Web.Controllers
 {
     public class Incoming_SmsController : ApiController
     {
+        private const string NameTag = "NAME";
+
         // GET: http://www.qcue.me/api/incoming_sms?to=447860033153&from=441234567890&content=Hello+World&msg_id=AB_12345
         public HttpResponseMessage Get([FromUri]IncomingSmsMessage message)
         {
@@ -28,60 +30,101 @@ namespace QCue.Web.Controllers
             var qbase = new QBase("https://qcue-live.firebaseio.com");
             var user = qbase.GetUserByMobileNumber(message.From);
 
-            // TODO: REFACTOR!
-            if (message.Content.ToUpper().StartsWith("NAME"))
+            this.EnsureUserIsKnown(message, user);
+
+            if (this.IsUpdateUserMessage(message))
             {
-                string fullName = message.Content.Substring(4).Trim();
-
-                user.fullName = fullName;
-
-                qbase.UpdateUser(user);
-
-                Trace.TraceInformation(
-                    "Updated user.fulName for \"{0}\" to \"{1}\"", user.userId, user.fullName);
+                this.UpdateUser(message, qbase, user);
             }
             else
             {
-
-                var q = qbase.GetQueueByShortCode(message.Content);
-
-                if (q == null)
-                {
-                    string errorMessage = String.Format(
-                        "No such queue: \"{0}\".", message.Content);
-
-                    var invalidQueueResponse = new HttpResponseMessage(HttpStatusCode.BadRequest)
-                    {
-                        Content = new StringContent(errorMessage),
-                        ReasonPhrase = errorMessage
-                    };
-
-                    throw new HttpResponseException(invalidQueueResponse);
-                }
-
-                string status = null;
-
-                if (user == null)
-                {
-                    user = qbase.AddAnonymousUser(message.From);
-                    // TODO: status = "registering";
-                    status = "joined";
-                }
-                else
-                {
-                    status = "joined";
-                }
-
-                qbase.AddUserToQueue(q.queueId, new QUser
-                {
-                    userId = user.userId,
-                    status = status
-                });
+                user = this.QueueUser(message, qbase, user);
             }
 
             var response = this.Request.CreateResponse(HttpStatusCode.OK);
 
             return response;
+        }
+
+        private void EnsureUserIsKnown(IncomingSmsMessage message, Models.User user)
+        {
+            if (user == null)
+            {
+                string errorMessage = String.Format(
+                    "No such user: \"{0}\".", message.From);
+
+                var response = new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent(errorMessage),
+                    ReasonPhrase = errorMessage
+                };
+
+                Trace.TraceError(errorMessage);
+                throw new HttpResponseException(response);
+            }
+        }
+
+        private User QueueUser(IncomingSmsMessage message, QBase qbase, User user)
+        {
+            var q = qbase.GetQueueByShortCode(message.Content);
+
+            this.EnsureQueueIsKnown(message, q);
+
+            string status = null;
+
+            if (user == null)
+            {
+                user = qbase.AddAnonymousUser(message.From);
+                // TODO: status = "registering";
+                status = "joined";
+            }
+            else
+            {
+                status = "joined";
+            }
+
+            qbase.AddUserToQueue(q.queueId, new QUser
+            {
+                userId = user.userId,
+                status = status
+            });
+
+            return user;
+        }
+
+        private void EnsureQueueIsKnown(IncomingSmsMessage message, Q q)
+        {
+            if (q == null)
+            {
+                string errorMessage = String.Format(
+                    "No such queue: \"{0}\".", message.Content);
+
+                var response = new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent(errorMessage),
+                    ReasonPhrase = errorMessage
+                };
+
+                Trace.TraceError(errorMessage);
+                throw new HttpResponseException(response);
+            }
+        }
+
+        private void UpdateUser(IncomingSmsMessage message, QBase qbase, User user)
+        {
+            string fullName = message.Content.Substring(NameTag.Length).Trim();
+
+            user.fullName = fullName;
+
+            qbase.UpdateUser(user);
+
+            Trace.TraceInformation(
+                "Updated user.fulName for \"{0}\" to \"{1}\"", user.userId, user.fullName);
+        }
+
+        private bool IsUpdateUserMessage(IncomingSmsMessage message)
+        {
+            return message.Content.ToUpper().StartsWith(NameTag);
         }
     }
 }
