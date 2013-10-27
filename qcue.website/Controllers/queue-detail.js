f590e8ae62e4dc94abeb9950c3047d1e7093acf6
Craig Reay <craigcsruk@googlemail.com>
2013-10-27 12:32:48 +0000

    Most of the changes to get speak js working. There are a few bugs.

--- qcue.website/Controllers/queue-detail.js
+++ qcue.website/Controllers/queue-detail.js
@@ -1,5 +1,25 @@
-﻿function QueueDetailController($scope, $routeParams, angularFire) {
-    var ref = new Firebase('https://qcue-live.firebaseio.com/queues/' + $routeParams.id);
+﻿function QueueDetailController($scope, $routeParams, $log, angularFire, angularFireCollection) {
+
+    var ref = new Firebase("https://qcue-live.firebaseio.com/queues/" + $routeParams.id);
     $scope.queue = null;
     angularFire(ref, $scope, "queue");
+
+    //$scope.$watch('queue', function (newQueueValue, oldQueueValue) {
+    //    if (newQueueValue) {
+    //        for (var key in newQueueValue.users) {
+    //            $scope.$watch('queue.users["'+key+'"].status', function (newVal, oldVal) {
+    //                if (newVal != oldVal && newVal == 'servicing') {
+    //                    var user = $scope.queue.users[key];
+
+    //                    var url = new Firebase("https://qcue-live.firebaseio.com/users/" + user.userId);
+    //                    var randomReturn = angularFireCollection(url, function (fullUserDetails) {
+    //                        if (fullUserDetails.val()) {
+    //                            speak(fullUserDetails.val().fullName + 'someone is now next in the queue.');
+    //                        }
+    //                    });
+    //                }
+    //            });
+    //        }
+    //    }
+    //});
 }
