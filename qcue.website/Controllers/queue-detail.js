77be07462b449c96d4f6b2a7a15a3bdc9a2e9802
Alan Gorton <dev.code1up@googlemail.com>
2013-11-22 12:35:34 +0000

    sssss

--- qcue.website/Controllers/queue-detail.js
+++ qcue.website/Controllers/queue-detail.js
@@ -4,22 +4,22 @@
     $scope.queue = null;
     angularFire(ref, $scope, "queue");
 
-    //$scope.$watch('queue', function (newQueueValue, oldQueueValue) {
-    //    if (newQueueValue) {
-    //        for (var key in newQueueValue.users) {
-    //            $scope.$watch('queue.users["'+key+'"].status', function (newVal, oldVal) {
-    //                if (newVal != oldVal && newVal == 'servicing') {
-    //                    var user = $scope.queue.users[key];
+    $scope.$watch('queue', function (newQueueValue, oldQueueValue) {
+        if (newQueueValue) {
+            for (var key in newQueueValue.users) {
+                $scope.$watch('queue.users["'+key+'"].status', function (newVal, oldVal) {
+                    if (newVal != oldVal && newVal == 'servicing') {
+                        var user = $scope.queue.users[key];
 
-    //                    var url = new Firebase("https://qcue-live.firebaseio.com/users/" + user.userId);
-    //                    var randomReturn = angularFireCollection(url, function (fullUserDetails) {
-    //                        if (fullUserDetails.val()) {
-    //                            speak(fullUserDetails.val().fullName + 'someone is now next in the queue.');
-    //                        }
-    //                    });
-    //                }
-    //            });
-    //        }
-    //    }
-    //});
+                        var url = new Firebase("https://qcue-live.firebaseio.com/users/" + user.userId);
+                        var randomReturn = angularFireCollection(url, function (fullUserDetails) {
+                            if (fullUserDetails.val()) {
+                                speak(fullUserDetails.val().fullName + 'someone is now next in the queue.');
+                            }
+                        });
+                    }
+                });
+            }
+        }
+    });
 }
